d8cab21a101a4735c1278bb8084ea36d6727f5e0
John Thompson <john@springframework.guru>
2022-12-28 10:16:25 -0500

    initial project add

---
+++ src/main/java/guru/springframework/spring6reactive/Spring6ReactiveApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring6reactive/Spring6ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
