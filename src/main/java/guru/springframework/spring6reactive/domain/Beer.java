974e68a6ab79ea6cd8803f6efd5b1724489ae28a
John Thompson <john@springframework.guru>
2022-12-28 11:10:02 -0500

    adding beer entity

---
+++ src/main/java/guru/springframework/spring6reactive/domain/Beer.java
@@ -0,0 +1,31 @@
+package guru.springframework.spring6reactive.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+
+    @Id
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
+}
