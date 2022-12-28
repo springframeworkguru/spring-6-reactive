60629890248a846c02f745e77b0c6367f0a228b6
John Thompson <john@springframework.guru>
2022-12-28 10:58:43 -0500

    adding database init

---
+++ src/main/java/guru/springframework/spring6reactive/config/DatabaseConfig.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring6reactive.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Configuration
+public class DatabaseConfig {
+
+    @Value("classpath:/schema.sql")
+    Resource resource;
+
+    @Bean
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory){
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(resource));
+        return initializer;
+    }
+}
