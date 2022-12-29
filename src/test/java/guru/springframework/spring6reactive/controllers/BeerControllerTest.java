d834d30cde80042cbaf4a436ab0cc4d9bb93c57d
John Thompson <john@springframework.guru>
2022-12-29 17:00:34 -0500

    add test list beers

---
+++ src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring6reactive.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class BeerControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void testListBeers() {
+        webTestClient.get().uri(BeerController.BEER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody().jsonPath("$.size()").isEqualTo(3);
+    }
+}
