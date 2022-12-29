a03d5080ee1e2847c53c2df504d3c6848dd4d6be
John Thompson <john@springframework.guru>
2022-12-29 11:00:15 -0500

    adding test for postman testing

--- src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
@@ -1,5 +1,7 @@
 package guru.springframework.spring6reactive.repositories;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6reactive.config.DatabaseConfig;
 import guru.springframework.spring6reactive.domain.Beer;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,13 @@ class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testCreateJson() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        System.out.println(objectMapper.writeValueAsString(getTestBeer()));
+    }
 
     @Test
     void saveNewBeer() {
