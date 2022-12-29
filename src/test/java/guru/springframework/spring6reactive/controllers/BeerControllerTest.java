2dd218e2d97d5c965a5c48ab0177d8579be01d36
John Thompson <john@springframework.guru>
2022-12-29 17:08:59 -0500

    test get by id

--- src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6reactive.controllers;
 
+import guru.springframework.spring6reactive.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -14,6 +15,15 @@ class BeerControllerTest {
 
     @Autowired
     WebTestClient webTestClient;
+
+    @Test
+    void testGetById() {
+        webTestClient.get().uri(BeerController.BEER_PATH_ID, 1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody(BeerDTO.class);
+    }
 
     @Test
     void testListBeers() {
